df5322f76485f17b8be3da68d7741f2faa3a7b1f
Danielessu <danielessu@unisabana.edu.co>
2026-04-15 15:50:02 -0500

    Refactor motor control and interrupt handling
    Refactor code to improve interrupt handling and ADC reading. Added locking mechanism for button events and updated motor control logic.

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -1,101 +1,143 @@
-// ======== PINES ========
-const int potPin = A0;
-
-const int pwmPin = 9;
-const int in1 = 4;
-const int in2 = 5;
-
-const int btnStop = 2;
-const int btnDir = 3;
-
-const int ledStop = 12;
-const int ledDir = 13;
-const int ledPWM = 10;
+#include <Arduino.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdbool.h>
 
 // ======== VARIABLES ========
 volatile bool stopEvent = false;
-volatile bool dirEvent = false;
+volatile bool dirEvent  = false;
 
-bool motorOn = true;
+bool motorOn   = true;
 bool direction = true;
 
+// 🔥 NUEVO: control de bloqueo
+bool stopLocked = false;
+bool dirLocked  = false;
+
 unsigned long lastStop = 0;
-unsigned long lastDir = 0;
+unsigned long lastDir  = 0;
+
 const int debounce = 200;
 
 // ======== INTERRUPCIONES ========
-void isrStop() {
-  stopEvent = true;
+ISR(INT0_vect) {
+  if (!(PIND & (1 << PD2))) { // botón presionado
+    stopEvent = true;
+  }
 }
 
-void isrDir() {
-  dirEvent = true;
+ISR(INT1_vect) {
+  if (!(PIND & (1 << PD3))) {
+    dirEvent = true;
+  }
+}
+
+// ======== ADC ========
+void setupADC() {
+  ADMUX  = (1 << REFS0);
+  ADCSRA = (1 << ADEN) |
+           (1 << ADPS2) |
+           (1 << ADPS1) |
+           (1 << ADPS0);
+}
+
+uint16_t readADC() {
+  ADCSRA |= (1 << ADSC);
+  while (ADCSRA & (1 << ADSC));
+  return ADCW;
+}
+
+// ======== PWM ========
+void setupTimer1() {
+  TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << WGM10);
+  TCCR1B = (1 << WGM12)  | (1 << CS11)   | (1 << CS10);
+
+  OCR1A = 0;
+  OCR1B = 0;
 }
 
 // ======== SETUP ========
 void setup() {
   Serial.begin(115200);
-  
-  pinMode(pwmPin, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
 
-  pinMode(btnStop, INPUT_PULLUP);
-  pinMode(btnDir, INPUT_PULLUP);
+  DDRB |= (1 << PB1) | (1 << PB2) | (1 << PB4) | (1 << PB5);
 
-  pinMode(ledStop, OUTPUT);
-  pinMode(ledDir, OUTPUT);
-  pinMode(ledPWM, OUTPUT);
+  DDRD |= (1 << PD4) | (1 << PD5);
+  DDRD &= ~((1 << PD2) | (1 << PD3));
 
-  attachInterrupt(digitalPinToInterrupt(btnStop), isrStop, FALLING);
-  attachInterrupt(digitalPinToInterrupt(btnDir), isrDir, FALLING);
-  
+  PORTD |= (1 << PD2) | (1 << PD3);
+
+  EICRA |= (1 << ISC01) | (1 << ISC11);
+  EICRA &= ~((1 << ISC00) | (1 << ISC10));
+
+  EIMSK |= (1 << INT0) | (1 << INT1);
+
+  setupADC();
+  setupTimer1();
+
+  sei();
+
   Serial.println("Sistema iniciado");
+}
+
+// ======== DIRECCIÓN ========
+void setMotorDirection(bool dir) {
+  if (dir) {
+    PORTD |=  (1 << PD4);
+    PORTD &= ~(1 << PD5);
+  } else {
+    PORTD &= ~(1 << PD4);
+    PORTD |=  (1 << PD5);
+  }
 }
 
 // ======== LOOP ========
 void loop() {
 
-  // --- BOTÓN STOP ---
-  if (stopEvent && millis() - lastStop > debounce) {
-    motorOn = !motorOn;
-    stopEvent = false;
-    lastStop = millis();
-    Serial.println(motorOn ? "Motor ON" : "Motor OFF");
-  }
+  // -------- BOTÓN STOP (ANTI DOBLE CLICK) --------
+  if (!(PIND & (1 << PD2))) {  // presionado
+    if (!stopLocked && millis() - lastStop > debounce) {
+      motorOn = !motorOn;
+      lastStop = millis();
+      stopLocked = true;
 
-  // --- BOTÓN DIRECCIÓN ---
-  if (dirEvent && millis() - lastDir > debounce) {
-    direction = !direction;
-    dirEvent = false;
-    lastDir = millis();
-    Serial.println(direction ? "Horario" : "Antihorario");
-  }
-
-  // --- LECTURA POTENCIÓMETRO ---
-  int potValue = analogRead(potPin);
-  int pwmValue = map(potValue, 0, 1023, 0, 255);
-  Serial.println(pwmValue);
-
-  // LED indicador de intensidad
-  analogWrite(ledPWM, pwmValue);
-
-  // --- CONTROL MOTOR ---
-  if (!motorOn) {
-    analogWrite(pwmPin, 0);
-  } else {
-    analogWrite(pwmPin, pwmValue);
-
-    if (direction) {
-      digitalWrite(in1, HIGH);
-      digitalWrite(in2, LOW);
-    } else {
-      digitalWrite(in1, LOW);
-      digitalWrite(in2, HIGH);
+      Serial.println(motorOn ? "Motor ON" : "Motor OFF");
     }
+  } else {
+    stopLocked = false; // se liberó el botón
   }
 
-  // --- LEDs DE ESTADO ---
-  digitalWrite(ledStop, !motorOn);
-  digitalWrite(ledDir, direction);
+  // -------- BOTÓN DIRECCIÓN --------
+  if (!(PIND & (1 << PD3))) {
+    if (!dirLocked && millis() - lastDir > debounce) {
+      direction = !direction;
+      lastDir = millis();
+      dirLocked = true;
+
+      Serial.println(direction ? "Horario" : "Antihorario");
+    }
+  } else {
+    dirLocked = false;
+  }
+
+  // -------- POTENCIÓMETRO --------
+  uint16_t potValue = readADC();
+  uint8_t pwmValue  = (potValue * 255UL) / 1023;
+
+  OCR1B = pwmValue;
+
+  // -------- MOTOR --------
+  if (!motorOn) {
+    OCR1A = 0;
+  } else {
+    OCR1A = pwmValue;
+    setMotorDirection(direction);
+  }
+
+  // -------- LEDs --------
+  if (!motorOn) PORTB |=  (1 << PB4);
+  else          PORTB &= ~(1 << PB4);
+
+  if (direction) PORTB |=  (1 << PB5);
+  else           PORTB &= ~(1 << PB5);
 }
